ebe1afab3b8e1091a146eb8586e478c43cf425af
liaosiwei <liaosiweiorxiaowei@gmail.com>
2013-07-30 22:06:39 +0800

    Signed-off-by: liaosiwei <liaosiweiorxiaowei@gmail.com>

--- pychseg2/mmseg/simplematch.py
+++ pychseg2/mmseg/simplematch.py
@@ -6,6 +6,7 @@ Created on 2013-7-30
 '''
 
 from utils.dbquery import DatabaseQuery
+import time
 
 def get_chunk(file, num):
     return file.read(num)
@@ -24,7 +25,7 @@ def simple_match(file):
             word_read_len = word_len
             yield chunk
         else:
-            for i in range(len(chunk)-1):
+            for i in range(len(chunk)-1):  # 复杂度 O(n2)
                 if query.isbigram(chunk[:-1-i]):
                     word_read_len = word_len - i - 1
                     yield chunk[:-1-i]
@@ -45,9 +46,15 @@ if __name__ == '__main__':
     import io
     sample_str = '''习近平指出，要坚决贯彻落实中央和军委有关作风建设规定，持之以恒、锲而不舍，善始善终、善做善成，不断把作风建设引向深入，以良好作风推动部队全面建设。要认真落实标准更高、走在前列的要求，扎扎实实抓好党的群众路线教育实践活动，深入基层、深入官兵，广泛听取意见建议，搞好专项整治。各级党委和领导既是组织者也是参与者，要坚持以上率下，把自己摆进去，从自身严起，从现在改起，从小事抓起，用实际行动为部队做好样子。要组织好团以上领导和机关干部下连当兵、蹲连住班，通过这种方式改进工作作风、密切官兵关系、加强基层建设，把部队基础打得更加牢固。'''
     strs = io.StringIO(sample_str)
-
+    start_cpu = time.clock()
+    start_real = time.time()
     res = simple_match(strs)
     for word in res:
         print (word)
+    end_cpu = time.clock()
+    end_real = time.time()
+    print("%f real seconds" % (end_real - start_real))
+    print("%f CPU seconds" % (end_cpu - start_cpu))
+    print("processing speed is %f" % (float(len(sample_str))/(end_real - start_real)))
     strs.close()
     
